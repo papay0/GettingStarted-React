358f2ffeab4482b952a5acf6949895067c7ca0d6
Arthur Papailhau <papailhau@gmail.com>
2016-07-04 09:13:27 +0100

    add EmojiReact

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ npm start
 ```
 
 to produce `bundle.js` with example code and React.
+
+If you want to install a package from github, you can use this command:
+`npm install https://github.com/papay0/react-emoji-react --save`
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import EmojiReact from 'react-emoji-react';
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
@@ -10,8 +12,63 @@ var ExampleApplication = React.createClass({
     return <p>{message}</p>;
     }
   });
-  
+
+  const emojis = [
+  {
+    name: 'rage',
+    count: 2
+  },
+  {
+    name: 'blush',
+    count: 1
+  },
+  {
+    name: 100,
+    count: 3
+  },
+  {
+    name: 'grinning',
+    count: 2
+  }
+];
+
+class ReactingComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emojis
+    };
+  }
+
+  onReaction(name) {
+    const emojis = this.state.emojis.map(emoji => {
+      if (emoji.name === name) {
+        emoji.count += 1;
+      }
+      return emoji;
+    });
+    this.setState({ emojis });
+  }
+
+  onEmojiClick(name) {
+    console.log(name);
+    const emojis = this.state.emojis.concat([{name, count: 1}]);
+    this.setState({ emojis });
+  }
+
+  render() {
+    return (
+      <EmojiReact
+        reactions={this.state.emojis}
+        onReaction={(name) => this.onReaction(name)}
+        onEmojiClick={(name) => this.onEmojiClick(name)}
+        sort={true}
+      />
+    );
+  }
+}
+
   ReactDOM.render(
-    <ExampleApplication />,
+    <ReactingComponent />,
     document.getElementById('app')
   );
